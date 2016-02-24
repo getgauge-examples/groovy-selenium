1ce519bfc210f64a48957089460e6c2de7022ba3
apoorvam <apoorva_rarefriend@yahoo.com>
2016-02-24 11:06:05 +0530

    adding src/test/groovy in project structure | adding gmavenplus-plugin in pom

--- src/test/groovy/com/thoughtworks/gauge/example/pages/BasePage.groovy
+++ src/test/groovy/com/thoughtworks/gauge/example/pages/BasePage.groovy
@@ -15,32 +15,32 @@ To use the examples locally you would need to follow these steps:
 public abstract class BasePage {
     protected static String Url = System.getenv("APP_ENDPOINT")
 
-    public void storeStringToScenarioDataStore(String key, String value) {
+    public static void storeStringToScenarioDataStore(String key, String value) {
         DataStore scenarioStore = DataStoreFactory.getScenarioDataStore()
         scenarioStore.put(key, value)
     }
 
-    public void storeStringToSpecDataStore(String key, String value) {
+    public static void storeStringToSpecDataStore(String key, String value) {
         DataStore specStore = DataStoreFactory.getSpecDataStore()
         specStore.put(key, value)
     }
 
-    public void storeStringToSuiteDataStore(String key, String value) {
+    public static void storeStringToSuiteDataStore(String key, String value) {
         DataStore suiteStore = DataStoreFactory.getSuiteDataStore()
         suiteStore.put(key, value)
     }
 
-    public String fetchStringFromScenarioDataStore(String key) {
+    public static String fetchStringFromScenarioDataStore(String key) {
         DataStore scenarioStore = DataStoreFactory.getScenarioDataStore()
         return (String) scenarioStore.get(key)
     }
 
-    public String fetchStringFromSpecDataStore(String key) {
+    public static String fetchStringFromSpecDataStore(String key) {
         DataStore specStore = DataStoreFactory.getSpecDataStore()
         return (String) specStore.get(key)
     }
 
-    public String fetchStringFromSuiteDataStore(String key) {
+    public static String fetchStringFromSuiteDataStore(String key) {
         DataStore suiteStore = DataStoreFactory.getSuiteDataStore()
         return (String) suiteStore.get(key)
     }
